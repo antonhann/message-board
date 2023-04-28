ad36f3837182f6bc28e67df2aa263b3ed1ef19c3
antonhann <antonha016@gmail.com>
2023-04-28 02:44:13 -0400

    added mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
 var express = require('express');
-const mongoose = require("mongoose")
+const message = require("./data/messages")
 var router = express.Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const connect = require("./data/mongo")
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date()
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date()
+//   }
+// ];
 // /* GET home page. */
-router.get('/', function(req, res, next) {
+// let messages = connect()
+// messages.then(() => {console.log(messages)})
+router.get('/', async function(req, res, next) {
+  let messages = await connect()
   res.render('index', {title: "Message Board", messages});
 });
 
@@ -24,13 +27,17 @@ router.get('/form', function(req, res, next) {
 });
 
 router.post("/form", function(req, res, next){
-  let message = req.body.message
+  let text = req.body.message
   let username = req.body.username
-  messages.push({
-    text: message,
+  message.create({
+    text: text,
     user: username,
-    added: new Date(),
   })
+  // messages.push({
+  //   text: message,
+  //   user: username,
+  //   added: new Date(),
+  // })
   res.redirect("/")
 })
 
